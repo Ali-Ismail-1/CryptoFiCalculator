8b5c1f98dfeee6d756ec3bb43ac1492e81571061
Ali <Ali@Servers4Less.com>
2022-10-03 20:15:27 -0700

    Successfully read array object in request.GET

--- calculatorapp/views.py
+++ calculatorapp/views.py
@@ -10,4 +10,17 @@ def index(request):
 @require_GET
 def calc(request):
     print("In the calculate function")
+
+    # equationArray = request.GET.get('equationArray')
+    equationArray = request.GET.getlist('equationArray[]')
+
+    print('Preview Data')
+    print(request.GET)
+    print(type(equationArray))
+    print(equationArray)
+
+    for x in equationArray:
+        print(x)
+
+    
     return HttpResponse('10')
