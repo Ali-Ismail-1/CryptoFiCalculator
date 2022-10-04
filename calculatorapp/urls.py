59864341a6e7d517891830d20acf4276275321b3
Ali <Ali@Servers4Less.com>
2022-10-03 19:40:07 -0700

    Testing ajax call

--- calculatorapp/urls.py
+++ calculatorapp/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),    
+    path('calculate', views.calculate, name="calculate")
 
 ]
