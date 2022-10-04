ce1995ad1711896e4b95e640b13791111d2ded24
Ali <Ali@Servers4Less.com>
2022-10-03 21:18:06 -0700

    successfully passed test cases with float numbers

--- calculatorapp/views.py
+++ calculatorapp/views.py
@@ -8,6 +8,13 @@ def index(request):
     return render(request, 'index.html')
 
 
+def is_number(string):
+    try:
+        float(string)
+        return True
+    except ValueError:
+        return False
+
 @require_GET
 def calc(request):
     print("In the calculate function")
@@ -27,28 +34,30 @@ def calc(request):
     operator = ''
     current = 0
     for i in range(1, len(equationArray), 1):
-        equationArray[i]
-        if(equationArray[i].isnumeric() == False):
+        #equationArray[i]
+        if (is_number(equationArray[i]) == False):
             operator = equationArray[i]
         elif (operator != ''):
             
+            print("The Current Number is: " + str(current))            
             current = float(equationArray[i])
-            print(current)
+            print("The Current Number is: " + str(current))            
                 
 
             if (operator == "+"):
                 total += current
             elif (operator == "-"):
                 total -= current
-            elif (operator == "x"):
+            elif (operator == "x"):                
+                print(total)
+                print(current)
+                print(total * current)
                 total *= current
             elif (operator == "/"):                
                 if current == 0: # handle divide by zero
                     total = 0
                 else:
-                    total /= current
-            elif (operator == "FLIP"):
-                total *= -1
+                    total /= current            
             operator = ''
             
     
